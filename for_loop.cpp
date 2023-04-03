a5d97dd06b3119b9f2cbd17d96ba4e2d293c5a8e
FIRST_NAME LAST_NAME <jsdfgs>
2023-04-03 12:38:53 +0530

    first commit

---
+++ for_loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cin >> a >> b;
+    
+    string words[] = {"one", "two", "three", "four", "five",
+                        "six", "seven", "eight", "nine"};
+    
+    for(int n = a; n<=b; n++){
+        
+        if(n >= 1 && n <= 9){
+        cout << words[n-1]<<"\n";
+        } else if (n%2==1){
+            cout << "odd"<<"\n";
+        } else {
+            cout << "even"<<"\n";
+        }
+    }
+    
+    return 0;
+}
